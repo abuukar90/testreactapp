e97b97d88a5d9c47c139e07c4e9c447a9407a794
Abuukar <abuu01@osumc.edu>
2022-12-01 00:46:51 -0500

    added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import "./index.css";
+function BookList(){
+  return( 
+<section>
+  <Book/>
+  <Book/>
+  <Book/>
+</section>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Book(){
+  return (
+    <article> 
+<Image/>
+<Title/>
+<Autor/>
+   </article>
+   
+            
+  );
+}
+
+function Image(){
+  return(
+    <img src= "https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg" alt=""/>
+  );
+}
+
+function Title (){
+return(
+<h1> i love books</h1>
 );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Autor (){
+  return(
+<h4> Abuukar</h4>
+  );
+}
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world');
+// };
+ReactDom.render(<BookList  />,  document.getElementById('root'));
+//ReactDom.render(<Person />, document.getElementById('root'));
